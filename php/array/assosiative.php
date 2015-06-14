1d2503ef13773f72c43ff443c910441d3799680b
Dikabaka <Andhy.anu@gmail.com>
2015-06-14 19:35:28 +0800

    menambahkan assosiative n multidimensi

---
+++ php/array/assosiative.php
@@ -0,0 +1,10 @@
+<?php
+    $komik['onepiece'] = "onepiece";
+    $komik['codegeass'] = "codegeass";
+    $komik['dragonball'] = "dragonball";
+    
+    echo "Komik ini ada : ".$komik['onepiece']. "\n";
+    echo "komik ini ada : ".$komik['codegeass']. "\n";
+    echo "komik ini ada : ".$komik['dragonball']. "\n";
+    
+?>
---
+++ php/array/multidimensi.php
@@ -0,0 +1,20 @@
+<?php
+
+	$sosmed = array(
+		'facebook' => array(
+		'name' => 'facebook',
+		'url' => 'http://www.facebook.com',
+		),
+ 		'twitter' => array(
+ 		'name' => 'Twitter',
+ 		'url' => 'http://www.twitter.com',
+ 		),
+ 		'gmail' => array(
+ 		'name' => 'gmail',
+ 		'url' => 'http://www.gmail.com',
+ 		),
+	);
+
+echo $sosmed['gmail']['url'];
+
+?>
